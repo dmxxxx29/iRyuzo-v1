4b76b0aa8ec77e2eb77754fcee8a1af01168cbbb
dmxxxx29 <daymixwashere@gmail.com>
2025-08-14 23:13:28 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,4 @@
+local ble = game:HttpGet("https://raw.githubusercontent.com/dmxxxx29/iRyuzo-v1/refs/heads/main/wey-bbe-ss.lua")
+local s, e = pcall(loadstring(ble))
+
+--please copy this and paste it into your executor.
